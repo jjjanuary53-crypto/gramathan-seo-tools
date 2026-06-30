8c7cab1471296bd054723b2c2a8d821569174e4b
jjjanuary53-crypto <jjjanuary53@gmail.com>
2026-06-30 15:17:38 +0530

    Created Gramathan SEO Tools Dashboard

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function analyzeSEO()   {
-    const url = document.getElementById("urlInput").ariaValueMax;
+    const url = document.getElementById("urlInput").value;
     if (url ==="") {
         alert("Please ente Website URL");
     return;
@@ -10,8 +10,9 @@ document.getElementById("descriptionResult").innerText = "checking...";
 document.getElementById("mobileResult").innerText = "checking...";
 setTimeout(() => {
     document.getElementById("score").innerText = "85/100";
-    document.getElementById("titleResult").innerText = "Meta Title optimmized";
+    document.getElementById("progress").style.width = "85%";
+    document.getElementById("titleResult").innerText = "Meta Title optimized";
     document.getElementById("descriptionResult").innerText = "Meta Description found";
-    DocumentTimeline.getElementById("mobileResult").innerText = "Mobile Friendly"; 
+    document.getElementById("mobileResult").innerText = "Mobile Friendly"; 
 }, 2000);
 }
